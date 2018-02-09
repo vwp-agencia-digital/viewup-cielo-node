811cb7e434cc6af3b0234fbac5f799ad54986b60
BrenoOliveira <breno@viewup.com.br>
2018-02-09 15:22:42 -0200

    Update

--- src/Ecommerce/CieloEcommerce.ts
+++ src/Ecommerce/CieloEcommerce.ts
@@ -5,7 +5,8 @@ import Environment from './Environment';
 import CreateSaleRequest from './Request/CreateSaleRequest';
 import QuerySaleRequest from './Request/QuerySaleRequest';
 import UpdateSaleRequest from './Request/UpdateSaleRequest';
-import TokenizeCardRequest from './Request/TokenizeCardRequest';
+import {consoleTestResultHandler} from "tslint/lib/test";
+// import TokenizeCardRequest from './Request/TokenizeCardRequest';
 
 export default class CieloEcommerce {
 
@@ -27,31 +28,31 @@ export default class CieloEcommerce {
         return await createSaleRequest.execute(sale);
     }
 
-    async getSale(paymentId: string){
+    async getSale(paymentId: string) {
         const querySaleRequest = new QuerySaleRequest(this._merchant, this._environment);
         return await querySaleRequest.execute(paymentId);
     }
 
-    cancelSale( paymentId: string, amount: number ){
-        const updateSaleRequest = new UpdateSaleRequest(null, this._merchant, this._environment);
+    async cancelSale(obj) {
+        const updateSaleRequest  =  new UpdateSaleRequest(null, this._merchant, this._environment);
 
-        updateSaleRequest.setAmount(amount);
-        return await updateSaleRequest.execute(paymentId);
+        updateSaleRequest.setAmount(obj.amount);
+        return await updateSaleRequest.execute(obj.paymentId);
+    }
+    //{paymentId: string, amount: number, serviceTaxAmount = null}
+    async captureSale(obj) {
+
+        const updateSaleRequest = new UpdateSaleRequest("capture", this._merchant, this._environment);
+
+        updateSaleRequest.setAmount(obj.amount);
+        updateSaleRequest.setServiceTaxAmount(obj.serviceTaxAmount);
+
+        return await updateSaleRequest.execute(obj.paymentId);
     }
 
-    captureSale(paymentId: string, amount: number, serviceTaxAmount = null){
-
-        const updateSaleRequest = new UpdateSaleRequest('capture', this._merchant, this._environment);
-
-        updateSaleRequest.setAmount(amount);
-        updateSaleRequest.setServiceTaxAmount(serviceTaxAmount);
-
-        return await updateSaleRequest.execute(paymentId);
-    }
-
-    tokenizeCard(card: CreditCard){
-        const tokenizeCardRequest = new TokenizeCardRequest(this._merchant. _this.environment);
-        return await tokenizeCardRequest.execute(card);
-    }
+    // tokenizeCard(card: CreditCard){
+    //     const tokenizeCardRequest = new TokenizeCardRequest(this._merchant. _this.environment);
+    //     return await tokenizeCardRequest.execute(card);
+    // }
 
 }
