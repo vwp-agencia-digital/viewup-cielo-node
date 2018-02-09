cd28bc4a5771a4cf3b2dd5e42e0aa7be17cd479e
Lucas-Viewup <lucas@viewup.com.br>
2018-02-09 15:24:11 -0200

    comment tokenize card request

--- src/Ecommerce/CieloEcommerce.ts
+++ src/Ecommerce/CieloEcommerce.ts
@@ -5,7 +5,7 @@ import Environment from './Environment';
 import CreateSaleRequest from './Request/CreateSaleRequest';
 import QuerySaleRequest from './Request/QuerySaleRequest';
 import UpdateSaleRequest from './Request/UpdateSaleRequest';
-import TokenizeCardRequest from './Request/TokenizeCardRequest';
+// import TokenizeCardRequest from './Request/TokenizeCardRequest';
 
 export default class CieloEcommerce {
 
@@ -29,29 +29,29 @@ export default class CieloEcommerce {
 
     async getSale(paymentId: string){
         const querySaleRequest = new QuerySaleRequest(this._merchant, this._environment);
-        return await querySaleRequest.execute(paymentId);
+        return querySaleRequest.execute(paymentId);
     }
 
-    cancelSale( paymentId: string, amount: number ){
+    async cancelSale( paymentId: string, amount: number ){
         const updateSaleRequest = new UpdateSaleRequest(null, this._merchant, this._environment);
 
         updateSaleRequest.setAmount(amount);
-        return await updateSaleRequest.execute(paymentId);
+        return updateSaleRequest.execute(paymentId);
     }
 
-    captureSale(paymentId: string, amount: number, serviceTaxAmount = null){
+    async captureSale(paymentId: string, amount: number, serviceTaxAmount = null){
 
         const updateSaleRequest = new UpdateSaleRequest('capture', this._merchant, this._environment);
 
         updateSaleRequest.setAmount(amount);
         updateSaleRequest.setServiceTaxAmount(serviceTaxAmount);
 
-        return await updateSaleRequest.execute(paymentId);
+        return updateSaleRequest.execute(paymentId);
     }
 
-    tokenizeCard(card: CreditCard){
-        const tokenizeCardRequest = new TokenizeCardRequest(this._merchant. _this.environment);
-        return await tokenizeCardRequest.execute(card);
-    }
+    // tokenizeCard(card: CreditCard){
+    //     const tokenizeCardRequest = new TokenizeCardRequest(this._merchant. _this.environment);
+    //     return await tokenizeCardRequest.execute(card);
+    // }
 
 }
