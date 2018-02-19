4e78de9aeb82b740e8ad8872cf744d6b064d5075
Washington Freitas <washington@viewup.com.br>
2018-02-19 11:34:08 -0300

    add remote change commiter

--- dist.sh
+++ dist.sh
@@ -1,19 +1,48 @@
 #!/usr/bin/env bash
+rm -rf ./dist
+mkdir -p ./dist;
+cd ./dist/
+echo "Add remote dist repository";
+git init;
+git remote add origin ssh://root@hospedaup.com.br:288/home/git/dist/viewup-cielo-node.git
+git pull origin master;
+cd ../
 
-if   !($(git remote | grep dist > /dev/null;)); then
-  echo "Add remote dist repository";
-  git remote add dist ssh://root@hospedaup.com.br:288/home/git/dist/viewup-cielo-node.git
-fi
-IGNORE=".idea
-yarn.lock
-src
-.gitlab-ci.yml
-package-lock.json
-node_modules
-*.log
-.vscode"
-echo $IGNORE > ./.gitignore;
+
+echo "Preparing files";
+
+npm run tsc-build;
+npm run document;
+
+
+cp -r ./lib/* ./dist;
+cp -r ./docs/ ./dist/docs;
+cp ./docs/README.md ./dist/README.md;
+cp ./package.json ./dist/package.json;
+cp ./typings.d.ts ./dist/typings.d.ts;
+
+echo "generate ignore";
+
+IGNORE=".idea\n
+yarn.lock\n
+src\n
+.gitlab-ci.yml\n
+package-lock.json\n
+node_modules\n
+*.log\n
+.vscode\n";
+echo $IGNORE > ./dist/.gitignore;
+
+echo "creating git local repository"
+cd ./dist/
+
+
 git add -A;
 git commit -m "generate version";
-git push dist --all;
-echo "done"
+git push origin master;
+
+echo "removing local temp files";
+cd ../
+rm -rf ./dist/
+
+echo "done";
